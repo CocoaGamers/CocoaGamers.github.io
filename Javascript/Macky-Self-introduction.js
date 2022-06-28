beea7520956ec98ca2aa8106c9fb434d7b7b167e
CocoaGamers <99920704+CocoaGamers@users.noreply.github.com>
2022-06-29 01:03:21 +0900

    Add files via upload

--- Javascript/Macky-Self-introduction.js
+++ Javascript/Macky-Self-introduction.js
@@ -5,14 +5,15 @@ let once3 = false;
 window.addEventListener('scroll', function () {
     taeget_position = document.querySelector('#target').getBoundingClientRect().top;
     taeget_position2 = document.querySelector('#target2').getBoundingClientRect().top;
+    taeget_position3 = document.querySelector('#target3').getBoundingClientRect().top;
     
     if (taeget_position <= window.innerHeight && once !== true) {
         once = true;
         const style = document.createElement('style')
         style.innerHTML = 
-            `#Yomomo{
+            `#contents1{
                 height: 150px;
-                background-color: orange;
+                background-color: #CCFF99;
             }
             .sample-text1 {
                 margin-left: 5%;
@@ -74,13 +75,13 @@ window.addEventListener('scroll', function () {
         once2 = true;
         const style = document.createElement('style')
         style.innerHTML = 
-            `#Macky{
+            `#Ycontents2{
                 height: 150px;
-                background-color: green;
+                background-color: #CCFF99;
             }
             .sample-text2 {
                 margin-left: 5%;
-                margin-top: 7%;
+                margin-top: 2%;
                 margin-bottom:  5%; 
                 animation-name: fadein;
                 animation-duration: 4s;
@@ -129,6 +130,70 @@ window.addEventListener('scroll', function () {
                 background-color: white;
             }
             #Icon2{
+                display: inline-block; 
+            }`
+        document.body.appendChild(style)
+    }
+    
+    if (taeget_position3 <= window.innerHeight && once3 !== true) {
+        once3 = true;
+        const style = document.createElement('style')
+        style.innerHTML = 
+            `#contents3{
+                height: 150px;
+                background-color: #CCFF99;
+            }
+            .sample-text3 {
+                margin-left: 5%;
+                margin-top: 2%;
+                margin-bottom:  5%; 
+                animation-name: fadein;
+                animation-duration: 4s;
+            }
+            @keyframes fadein {
+                from {
+                    opacity: 0;
+                    transform: translateY(20px);
+                }
+                to {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+            #HHH3{
+                text-align: center;
+                font-size: 20px;
+            }
+            #P3{
+                margin: 0 5% 5% 5%;
+                font-size: 12px;
+            }
+            .an2-fadeup3 {
+                font-weight: bold;
+                animation-name: fadeup;
+                animation-duration: 4s;
+            }
+            @keyframes fadeup {
+                from {
+                    opacity: 0;
+                    transform: translateY(20px);
+                }
+                to {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+            #contents3{
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                text-align: center;
+                width: 80%;
+                height: auto;
+                margin-left:10%;
+                background-color: white;
+            }
+            #Icon3{
                 display: inline-block; 
             }`
         document.body.appendChild(style)
--- Javascript/Yomomo-Self-introduction.js
+++ Javascript/Yomomo-Self-introduction.js
@@ -5,12 +5,13 @@ let once3 = false;
 window.addEventListener('scroll', function () {
     taeget_position = document.querySelector('#target').getBoundingClientRect().top;
     taeget_position2 = document.querySelector('#target2').getBoundingClientRect().top;
+    taeget_position3 = document.querySelector('#target3').getBoundingClientRect().top;
     
     if (taeget_position <= window.innerHeight && once !== true) {
         once = true;
         const style = document.createElement('style')
         style.innerHTML = 
-            `#Yomomo{
+            `#contents1{
                 height: 150px;
                 background-color: orange;
             }
@@ -74,7 +75,7 @@ window.addEventListener('scroll', function () {
         once2 = true;
         const style = document.createElement('style')
         style.innerHTML = 
-            `#Macky{
+            `#Ycontents2{
                 height: 150px;
                 background-color: orange;
             }
@@ -129,6 +130,70 @@ window.addEventListener('scroll', function () {
                 background-color: white;
             }
             #Icon2{
+                display: inline-block; 
+            }`
+        document.body.appendChild(style)
+    }
+    
+    if (taeget_position3 <= window.innerHeight && once3 !== true) {
+        once3 = true;
+        const style = document.createElement('style')
+        style.innerHTML = 
+            `#contents3{
+                height: 150px;
+                background-color: orange;
+            }
+            .sample-text3 {
+                margin-left: 5%;
+                margin-top: 2%;
+                margin-bottom:  5%; 
+                animation-name: fadein;
+                animation-duration: 4s;
+            }
+            @keyframes fadein {
+                from {
+                    opacity: 0;
+                    transform: translateY(20px);
+                }
+                to {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+            #HHH3{
+                text-align: center;
+                font-size: 20px;
+            }
+            #P3{
+                margin: 0 5% 5% 5%;
+                font-size: 12px;
+            }
+            .an2-fadeup3 {
+                font-weight: bold;
+                animation-name: fadeup;
+                animation-duration: 4s;
+            }
+            @keyframes fadeup {
+                from {
+                    opacity: 0;
+                    transform: translateY(20px);
+                }
+                to {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+            #contents3{
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                text-align: center;
+                width: 80%;
+                height: auto;
+                margin-left:10%;
+                background-color: white;
+            }
+            #Icon3{
                 display: inline-block; 
             }`
         document.body.appendChild(style)
